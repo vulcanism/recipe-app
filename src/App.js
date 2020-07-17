a05b0a86c7f5f17d93df280b767931647e929b5b
Savannah C <cynomania@gmail.com>
2020-07-17 14:16:20 -0400

    getRecipes test fetch

--- src/App.js
+++ src/App.js
@@ -4,23 +4,27 @@ import './App.css';
 function App() {
 
   const APP_ID = "749a6121";
-  const APP_KEY = "2ec1c0be635d04c9180f2ad885d45764";
-
-  const exampleReq = 
-    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const APP_KEY = "2ec1c0be635d04c9180f2ad885d45764"; 
 
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     console.log("Effect has been run");
-  });
+  }, []);     // Array is second argument for useEffect. Leaving empty means it runs once,
+                // when the page loads. Adding counter makes it run each time counter is clicked
+  
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = response.json()
+  }
   
   return (
     <div className="App">
       <form className="search-form">
         <input className="search-bar" type="text" />
-        <button onClick={() => setCounter(counter + 1)} className="search-button" type="submit">{counter}</button>
+        <button className="search-button" type="submit">Search</button>
       </form>
+      <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1>
     </div>
   );
 }
