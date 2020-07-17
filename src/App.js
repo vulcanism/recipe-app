8d35b977de3cb2028a9395c5962dc2f864a6ea79
Savannah C <cynomania@gmail.com>
2020-07-17 12:42:46 -0400

    set up test request

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
   const APP_ID = "749a6121";
   const APP_KEY = "2ec1c0be635d04c9180f2ad885d45764";
 
+  const exampleReq = 
+    `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
       <h1>Hello!</h1>
