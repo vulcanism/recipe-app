671d76cffb65d7d14a7909a21a9a606983fe5d17
Savannah C <cynomania@gmail.com>
2020-07-22 15:05:27 -0400

    rendering recipe component in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import Recipe from "./Components/Recipe"
+import Recipe from "./Components/Recipe";
 import './App.css';
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
+    console.log(data.hits);
   }
   
   return (
@@ -25,7 +26,10 @@ function App() {
       <form className="search-form">
         <input className="search-bar" type="text" />
         <button className="search-button" type="submit">Search</button>
-      </form>      
+      </form>
+      {recipes.map(recipe => (
+        <Recipe />
+      ))}      
     </div>
   );
 }
